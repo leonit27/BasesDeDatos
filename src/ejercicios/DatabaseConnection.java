06eb87dd90c891c50bf04a8a515e050575de77de
leonardo <leonit@alu.edu.gva.es>
2025-02-25 20:30:45 +0100

    Empezando ejercicios

---
+++ src/ejercicios/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://89.36.214.106:5432/f_1cfsy_0413f";
+    private static final String USER = "f_1cfsy_0413f";
+    private static final String PASSWORD = "f_1cfsy_0413f";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ src/ejercicios/Sentencia1.java
@@ -0,0 +1,7 @@
+package ejercicios;
+
+public class Sentencia1 {
+    public static void main(String[] args){
+
+    }
+}
