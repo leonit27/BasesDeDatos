14bfb8f9abca68ee7baa0ae296a6820335f50571
leonardo <leonit@alu.edu.gva.es>
2025-03-12 13:47:28 +0100

    Ejercicio SQLite finalizado

--- src/ejercicioSQLite/DatabaseConnectionPelis.java
+++ src/ejercicioSQLite/DatabaseConnectionPelis.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionPelis {
-    private static String URL = "jdbc:sqlite:BaseDeDatosPeliculas.db";
+    private static String URL = "jdbc:sqlite:BaseDeDatosPeliculas";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
--- src/ejercicioSQLite/Peliculas.java
+++ src/ejercicioSQLite/Peliculas.java
@@ -1,9 +1,8 @@
 package ejercicioSQLite;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import ejercicio2.DatabaseConnectionFac2;
 
+import java.sql.*;
 
 public class Peliculas {
     private String titulo;
@@ -20,11 +19,11 @@ public class Peliculas {
         this.rating = rating;
     }
 
-    public static void agregarPeliculas (String titulo, String director, int anyo, String genero, double rating) {
+    public static void agregarPeliculas(String titulo, String director, int anyo, String genero, double rating) {
         PreparedStatement st = null;
         Connection conn = null;
         
-        String sql = "INSERT INTO peliculas(titulo, director, año, genero, rating) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO peliculas(Título, Director, Año, Género, Rating) VALUES (?, ?, ?, ?, ?)";
         
         try {
             conn = DatabaseConnectionPelis.getConnection();
@@ -35,9 +34,10 @@ public class Peliculas {
             st.setInt(3, anyo);
             st.setString(4, genero);
             st.setDouble(5, rating);
-        }catch (SQLException ex){
+            st.executeUpdate();
+        } catch (SQLException ex){
             System.out.println("Error "+ ex.getMessage());
-        }finally {
+        } finally {
             try {
                 if (st != null && !st.isClosed()) st.close();
             } catch (SQLException ex) {
@@ -47,6 +47,147 @@ public class Peliculas {
                 if (conn != null && !conn.isClosed()) conn.close();
             } catch (SQLException ex) {
                 System.out.println("No se ha podido cerrar la connexion por alguna razón");
+            }
+        }
+    }
+
+    public static void mostrarPelicula(String titulo) {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DatabaseConnectionPelis.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM peliculas WHERE Título = '" + titulo + "'");
+
+            System.out.println("Título\t\t\tDirector\t\t\tAño\t\tGénero\t\tRating\n" +
+                    "----------------------------------------------------------------");
+
+            while (rs.next()) {
+                System.out.print(rs.getString(1) + "\t");
+                System.out.print(rs.getString(2) + "\t");
+                System.out.print(rs.getInt(3) + "\t");
+                System.out.print(rs.getString(4) + "\t");
+                System.out.println(rs.getDouble(5));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            try {
+                if (st != null && !st.isClosed()) st.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido un error al cerrar el Statement: " + ex.getMessage());
+            }
+
+            try {
+                if (conn != null && !conn.isClosed()) conn.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido un error al cerrar la conexión: " + ex.getMessage());
+            }
+
+            try {
+                if (rs != null && !rs.isClosed()) rs.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido un error al cerrar el ResultSet: " + ex.getMessage());
+            }
+        }
+    }
+
+    public static void modificarPelicula(String titulo, String director, int anyo, String genero, double rating) {
+        Connection conn = null;
+        Statement st = null;
+        String sql;
+
+        try {
+            conn = DatabaseConnectionPelis.getConnection();
+            st = conn.createStatement();
+
+            sql = "UPDATE peliculas SET Director = '" + director + "', Año = " + anyo + ", Género = '" + genero + "', Rating = " + rating +
+                    " WHERE Título = '" + titulo + "'";
+            st.executeUpdate(sql);
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            try {
+                if (st != null && !st.isClosed()) st.close();
+            } catch (SQLException ex) {
+                System.out.println("No se ha podido cerrar el Statement por alguna razón");
+            }
+            try {
+                if (conn != null && !conn.isClosed()) conn.close();
+            } catch (SQLException ex) {
+                System.out.println("No se ha podido cerrar la connexion por alguna razón");
+            }
+        }
+    }
+
+    public static void eliminarPelicula(String titulo) {
+        Connection conn = null;
+        Statement st = null;
+        String sql;
+
+        try {
+            conn = DatabaseConnectionPelis.getConnection();
+            st = conn.createStatement();
+
+            sql = "DELETE FROM peliculas WHERE Título = '" + titulo + "'";
+            st.executeUpdate(sql);
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            try {
+                if (st != null && !st.isClosed()) st.close();
+            } catch (SQLException ex) {
+                System.out.println("No se ha podido cerrar el Statement por alguna razón");
+            }
+            try {
+                if (conn != null && !conn.isClosed()) conn.close();
+            } catch (SQLException ex) {
+                System.out.println("No se ha podido cerrar la connexion por alguna razón");
+            }
+        }
+    }
+
+    public static void mostrarTabla() {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DatabaseConnectionPelis.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM peliculas");
+
+            System.out.println("Título\t\t\tDirector\t\t\tAño\t\tGénero\t\tRating\n" +
+                    "----------------------------------------------------------------");
+
+            while (rs.next()) {
+                System.out.print(rs.getString(1) + "\t");
+                System.out.print(rs.getString(2) + "\t");
+                System.out.print(rs.getInt(3) + "\t");
+                System.out.print(rs.getString(4) + "\t");
+                System.out.println(rs.getDouble(5));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            try {
+                if (st != null && !st.isClosed()) st.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido un error al cerrar el Statement: " + ex.getMessage());
+            }
+
+            try {
+                if (conn != null && !conn.isClosed()) conn.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido un error al cerrar la conexión: " + ex.getMessage());
+            }
+
+            try {
+                if (rs != null && !rs.isClosed()) rs.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido un error al cerrar el ResultSet: " + ex.getMessage());
             }
         }
     }
